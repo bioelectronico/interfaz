d046aaa8bcb85c953f825b99d712c6ae60455ab4
Hector Barajas <abarajase@gmail.com>
2016-05-23 21:26:40 -0500

    Added all activities from practica 1

--- week-02/Actividad-2.cpp
+++ week-02/Actividad-2.cpp
@@ -9,27 +9,30 @@ int main( int argc, char** argv )
 {
   if( argc != 2)
     {
-      cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
+      cout <<" Usage: display_image <Image>" << endl;
       return -1;
     }
+
   Mat image;
   image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
   // Read the file
   if(! image.data )
-    // Check for invalid input
-    {
+  {
       cout << "Could not open or find the image" << std::endl ;
       return -1;
-    }
-  namedWindow( "Display window1", WINDOW_AUTOSIZE );// Create a window for display.
-  namedWindow( "Display window2", WINDOW_AUTOSIZE );// Create a window for display.
-  namedWindow( "Display window3", WINDOW_AUTOSIZE );// Create a window for display.
+  }
+
+  namedWindow( "Display window1", WINDOW_AUTOSIZE );
+  namedWindow( "Display window2", WINDOW_AUTOSIZE );
+  namedWindow( "Display window3", WINDOW_AUTOSIZE );
+
   imshow( "Display window1", image );
   imshow( "Display window2", image );
   imshow( "Display window3", image );
 
-// Show our image inside it.
+  // Show our image inside it.
   waitKey(0);
-  
+
   return 0;
 }
